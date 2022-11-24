cccacd04befef9ef31613b2f536528be9c5d4f54
Anatolii Rubanenko <anatolii.rubanenko@metropolia.fi>
2022-11-24 15:12:28 +0200

    qwe

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -36,7 +36,7 @@ C_DEPS += \
 src/%.o: ../src/%.cpp src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C++ Compiler'
-	arm-none-eabi-c++ -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-c++ -std=c++11 -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

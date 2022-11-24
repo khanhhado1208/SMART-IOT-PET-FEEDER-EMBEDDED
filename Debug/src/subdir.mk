a73d64a1a3226ad3e3c2b636b073901b08c79b13
Jun Shen <juns@metropolia.fi>
2022-11-24 15:13:35 +0200

    Test pressure sensor

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -5,9 +5,14 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
 ../src/DigitalIoPin.cpp \
+../src/I2CSensor.cpp \
+../src/LpcUart.cpp \
+../src/Pressuresensor.cpp \
 ../src/cr_cpp_config.cpp \
 ../src/cr_startup_lpc15xx.cpp \
-../src/pet_feeder_project.cpp 
+../src/pet_feeder_project.cpp \
+../src/pressure_sensor.cpp \
+../src/retarget_uart.cpp 
 
 C_SRCS += \
 ../src/crp.c \
@@ -15,17 +20,27 @@ C_SRCS += \
 
 OBJS += \
 ./src/DigitalIoPin.o \
+./src/I2CSensor.o \
+./src/LpcUart.o \
+./src/Pressuresensor.o \
 ./src/cr_cpp_config.o \
 ./src/cr_startup_lpc15xx.o \
 ./src/crp.o \
 ./src/pet_feeder_project.o \
+./src/pressure_sensor.o \
+./src/retarget_uart.o \
 ./src/sysinit.o 
 
 CPP_DEPS += \
 ./src/DigitalIoPin.d \
+./src/I2CSensor.d \
+./src/LpcUart.d \
+./src/Pressuresensor.d \
 ./src/cr_cpp_config.d \
 ./src/cr_startup_lpc15xx.d \
-./src/pet_feeder_project.d 
+./src/pet_feeder_project.d \
+./src/pressure_sensor.d \
+./src/retarget_uart.d 
 
 C_DEPS += \
 ./src/crp.d \
@@ -33,17 +48,17 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.cpp src/subdir.mk
+src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C++ Compiler'
-	arm-none-eabi-c++ -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-c++ -std=c++11 -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\Users\Omistaja\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\Users\Omistaja\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-src/%.o: ../src/%.c src/subdir.mk
+src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -std=c11 -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\Users\ruban\Documents\MCUXpressoIDE_11.5.1_7266\workspace2\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -std=c11 -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\Users\Omistaja\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\Users\Omistaja\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- src/pet_feeder_project.cpp
+++ src/pet_feeder_project.cpp
@@ -15,11 +15,27 @@
 #include "board.h"
 #endif
 #endif
-
 #include <cr_section_macros.h>
 
 // TODO: insert other include files here
 #include "DigitalIoPin.h"
+#include "pressure_sensor.h"
+#include "Pressuresensor.h"
+static volatile int counter;
+void SysTick_Handler(void)
+{
+if(counter > 0) counter--;
+}
+#ifdef __cplusplus
+
+#endif
+void Sleep(int ms)
+{
+counter = ms;
+while(counter > 0) {
+__WFI();
+}
+}
 
 // TODO: insert other definitions and declarations here
 
@@ -38,22 +54,42 @@ int main(void) {
 #endif
 
     // TODO: insert code here
-    DigitalIoPin d10(0,27,DigitalIoPin::output);
-    DigitalIoPin d11(0,28,DigitalIoPin::output);
-    DigitalIoPin d12(0,12,DigitalIoPin::output);
-    DigitalIoPin d13(0,14,DigitalIoPin::output);
+    //DigitalIoPin d10(0,27,DigitalIoPin::output);
+    //DigitalIoPin d11(0,28,DigitalIoPin::output);
+    //DigitalIoPin d12(0,12,DigitalIoPin::output);
+    //DigitalIoPin d13(0,14,DigitalIoPin::output);
 
-
+    //DigitalIoPin switch1(0,17,DigitalIoPin::input, true);
+    //DigitalIoPin switch2(1,11,DigitalIoPin::input, true);
+    //DigitalIoPin switch3(1,9,DigitalIoPin::input, true);
     // Force the counter to be placed into memory
     volatile static int i = 0 ;
     // Enter an infinite loop, just incrementing a counter
+    Pressuresensor *pressure_sensor = new Pressuresensor();
+
+    pressure_sensor->start();
     while(1) {
+    	printf("%d",pressure_sensor->get_value());
 
-    	d10.write(true);
-    	d11.write(false);
 
-    	d12.write(true);
-    	d13.write(false);
+    	/*if(switch1.read()){
+    		d10.write(true);
+    		d11.write(false);
+    	}
+    	else if(switch2.read()){
+    	    		d10.write(false);
+    	    		d11.write(true);
+    	    	}*/
+
+    	//d12.write(true);
+    	//d13.write(false);
+    	/*while(!switch1.read());
+    	d10.write(false);
+    	    	d11.write(true);
+    	    	//d12.write(false);
+    	    	//d13.write(true);
+    	    	Sleep(5000);
+    	    	while(!switch2.read());*/
         i++ ;
         // "Dummy" NOP to allow source level single
         // stepping of tight while() loop
